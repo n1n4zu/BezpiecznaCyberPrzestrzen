0e788bff2739176ca87fcb36b452d0709c931cdb
KrI5s <kryspin.dziarekk@gmail.com>
2023-11-15 19:59:55 +0100

    dodanie pliku bazowego sstrony, podstron i css

--- strona/urls.py
+++ strona/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.glowna, name='glowna'),
+    path('', views.base, name='base'),
+    path('posty/', views.posty, name='posty'),
+    path('nauka/', views.nauka, name='nauka'),
+    path('kontakt/', views.kontakt, name='kontakt'),
 ]
--- strona/views.py
+++ strona/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
-def glowna(request):
-    return render(request, 'strona/glowna.html', {})
+
+def base(request):
+    return render(request, 'strona/base.html', {})
+
+
+def posty(request):
+    return render(request, 'strona/posty.html', {})
+
+
+def nauka(request):
+    return render(request, 'strona/nauka.html', {})
+
+
+def kontakt(request):
+    return render(request, 'strona/kontakt.html', {})
+
