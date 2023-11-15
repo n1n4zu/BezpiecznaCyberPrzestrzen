6b121b00eb45ce710421bfd91fca701c8ad648ad
KrI5s <kryspin.dziarekk@gmail.com>
2023-11-15 22:05:49 +0100

    zmiana pliku głównego

--- strona/urls.py
+++ strona/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
-    path('glowna/', views.glowna, name='glowna'),
+    path('', views.glowna, name='glowna'),
     path('posty/', views.posty, name='posty'),
     path('nauka/', views.nauka, name='nauka'),
     path('kontakt/', views.kontakt, name='kontakt'),
